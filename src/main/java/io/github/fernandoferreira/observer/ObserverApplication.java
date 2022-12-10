65661b59195301fec2f81a6fbd195c6a46987ae5
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2022-12-10 09:09:42 -0300

    Initial commit

---
+++ src/main/java/io/github/fernandoferreira/observer/ObserverApplication.java
@@ -0,0 +1,13 @@
+package io.github.fernandoferreira.observer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/fernandoferreira/observer/ObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fernandoferreira.observer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
